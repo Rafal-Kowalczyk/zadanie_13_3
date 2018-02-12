34fe04b948ddbf17f3335769053df0b141bd5cd9
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-02-12 10:17:26 +0100

    Add code

--- program.js
+++ program.js
@@ -1,2 +1,21 @@
-﻿
+﻿process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instuction = input.toString().trim();
+		switch(instuction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/vsNode':
+				process.stdout.write('Node version: '+ process.versions.node +'\n');
+				break;	
+			default:
+			process.stderr.write('Wrong instuction!');
+		};	
+	}
+});
+
 
